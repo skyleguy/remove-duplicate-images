6810d16e316decb6482e9331a83b9f810118bc2f
Kyle Lobsinger <kylelobsinger@Kyles-MacBook-Pro.local>
2021-01-05 10:03:49 -0500

    made a ton of helper functions to narrow down results

--- thing.py
+++ thing.py
@@ -6,13 +6,58 @@ from pathlib import Path
 from random import randint
 import shutil
 import time
-import _thread
+import re
 
 mypath = '/Users/kylelobsinger/Documents/remove-dupes/'
 initialLocation = 'photo_library'
-outPath = 'out'
+outPath = 'no-copies-movs'
 directoryList = listdir(mypath + initialLocation)
 processCount = 500
+pixelDistance = 3
+
+def findFileNamesWithParens():
+    if len(directoryList) > 1:
+        for fileName in directoryList:
+            if len(re.findall("\s\(\d\)", fileName)) > 0:
+                # print('found a copy! ' + fileName)
+                if re.sub("\s\(\d\)", "", fileName) in directoryList:
+                    # print('its original was in the list!')
+                    match = re.search("\s\(\d\)", fileName)
+                    # print(match.group(0))
+                    if ('1' in match.group(0)):
+                        # print('since this is the first copy, we\'ll copy it over')
+                        fileName = fileName.replace(match.group(0), "")
+                        # print('copying ' + fileName)
+                        copyObjToNewLocation({"biggest": fileName})
+                else:
+                    print(fileName + ': its original didnt exist!')
+
+def getFolderTypes():
+    types = []
+    if len(directoryList) > 1:
+        for fileName in directoryList:
+            extension = fileName.rsplit('.', 1)
+            if extension[1] not in types:
+                types.append(extension[1])
+    print(types)
+
+def numDocType(type):
+    count = 0
+    if len(directoryList) > 1:
+        for x in range(len(directoryList)):
+            if (type in directoryList[x].lower()):
+                count += 1
+    print('there are ' + str(count) + ' ' + type + ' documents here')
+
+def stripAllTypesOf(typeArr):
+    if len(directoryList) > 1:
+        for fileName in directoryList:
+            shouldCopy = True
+            for extension in typeArr:
+                if (extension in fileName.lower()):
+                    shouldCopy = False
+            if shouldCopy:
+                copyObjToNewLocation({'biggest': fileName})
 
 def loadObjects():
     start = time.time()
@@ -80,9 +125,16 @@ def areSamePictureRandomTimes(currObj, copyObj, randomTimes):
 
 def areSamePictureAllPixels(currObj, copyObj):
     for x in range(20):
-        if currObj['pixels'][x] != copyObj['pixels'][x]:
+        if not areCloseEnoughPixels(currObj['pixels'][x], copyObj['pixels'][x]):
             return False
     return True
+
+def areCloseEnoughPixels(pixelOne, pixelTwo):
+    if abs(pixelOne[0] - pixelTwo[0]) <= 2:
+        if abs(pixelOne[1] - pixelTwo[1]) <= 2:
+            if abs(pixelOne[2] - pixelTwo[2]) <= 2:
+                return True
+    return False
 
 def copyObjToNewLocation(obj):
     shutil.copy2(mypath + initialLocation + '/' + obj['biggest'], mypath + outPath + '/' + obj['biggest'])
@@ -124,9 +176,25 @@ def printSimilar(objects):
     copyObjToNewLocation(objects[-1])
     print("checking images", time.time() - start, "to run")
 
-tryToMakeDirectory()
-myObjects = loadObjects()
-print('started with: ' + str(len(myObjects)))
-printSimilar(myObjects)
-print('ended with: ' + str(len(listdir(mypath + '/' + outPath))))
-print('cut ' + str((len(myObjects) - len(listdir(mypath + '/' + outPath)))) + ' duplicates')
+# tryToMakeDirectory()
+# print('starting with ' + str(len(directoryList)) + ' objects')
+# myObjects = loadObjects()
+# print('started with: ' + str(len(myObjects)))
+# printSimilar(myObjects)
+# print('ended with: ' + str(len(listdir(mypath + '/' + outPath))))
+# print('cut ' + str((len(myObjects) - len(listdir(mypath + '/' + outPath)))) + ' duplicates')
+
+# for num doc types
+# numDocType('mp4')
+
+# to copy all files except a certain type into the out folder:
+# all possible values in photo_library stripAllTypesOf(['jp', 'heic', 'm4v', 'mov', 'png', 'mp4'])
+# tryToMakeDirectory()
+# stripAllTypesOf(['jp', 'm4v', 'heic', 'png', 'mp4'])
+
+# to get all types of files in folder:
+# getFolderTypes()
+
+#find all file names with (d) in it
+# tryToMakeDirectory()
+# findFileNamesWithParens()
